9c72a3f67477380ff50a4d93248b4b7ec3c45ad1
AL ZREIBI Jean <jalzreibi@murex.com>
2023-09-24 23:30:33 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # k8s-vagrant-virtualbox
 deploying k8 on virtualbox using vagrant
+
+
+Vagrant version: Vagrant 2.3.7
+Virtualbox version: Version 7.0.10 r158379 (Qt5.15.2)
+
+
+Go into each directory:
+1. run Vagrant up --> this will bring the VM up
+2. run Vagrant ssh --> this will allow you to ssh into the box
+3. run installk8.sh on each node --> this will upgrade the VM and install all the required tools and configuration
+4. restart the nodes and run sudo swapoff -a 
+
+On the master node:
+1. run sudo kubeadm init --pod-network-cidr=10.10.0.0/16 --apiserver-advertise-address=192.168.50.10 --> this will start the master node
+2. once the master node is up, run kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml --> this will install calico network CNI plugin
+3. run wget https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml --> this will download the custom resources file for the calico CNI
+4. edit the custom-resources.yaml, change CIDR to 10.0.0.0/16 and then run kubectl apply -f custom-resources.yaml
+5. wait until the master node goes into ready state
+
+On the worker nodes:
+1. run the kubeadmin join command outputed at the end of the kubeadmin init command it will look something like this
+kubeadm join 192.168.50.10:6443 --token squ6en.7dqxhbtsksa226vw \
+--discovery-token-ca-cert-hash sha256:a9ea616399415226d7721a970f3627afe9ef0b81b55595b9bf7403a1e98d52a4
+
+
+Wait for the nodes to go into Ready state.
+
+Congrats! you just installed your k8 cluster!
---
+++ intsallk8.sh
@@ -0,0 +1,67 @@
+#/bin/bash
+
+apt update && apt upgrade -y
+sudo mkdir -m 755 /etc/apt/keyrings
+swapoff -a
+apt remove containerd
+
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay && sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+# Add k8 repository and install kubelet kubeadm kubectl
+sudo apt-get install -y apt-transport-https ca-certificates gnupg lsb-release
+curl -Ls "https://sbom.k8s.io/$(curl -Ls https://dl.k8s.io/release/stable.txt)/release" | grep "SPDXID: SPDXRef-Package-registry.k8s.io" |  grep -v sha256 | cut -d- -f3- | sed 's/-/\//' | sed 's/-v1/:v1/'
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt update
+curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+apt update
+
+sudo apt-get install -y kubelet=1.27.6-00 kubeadm=1.27.6-00 kubectl=1.27.6-00
+
+# Add Docker's official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# install containderd
+sudo apt-get install containerd.io
+
+# configure containerd to work with systemd
+cp /etc/containerd/config.toml /etc/containerd/config.toml.BAK
+cat << EOF > /etc/containerd/config.toml
+version = 2
+[plugins]
+  [plugins."io.containerd.grpc.v1.cri"]
+   [plugins."io.containerd.grpc.v1.cri".containerd]
+      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+          runtime_type = "io.containerd.runc.v2"
+          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+            SystemdCgroup = true
+EOF
+
+sudo systemctl restart containerd
